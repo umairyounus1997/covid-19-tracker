37534103575cf48b21e2ee829ac11906a45a274b
Umair Younus <41501844+umairyounus1997@users.noreply.github.com>
2020-07-23 23:12:30 +0500

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,9 +11,29 @@ const Cards = (props)=>{
             <Grid item component = {Card}>
             <CardContent>
             <Typography color = "textSecondary" gutterBottom>Infected</Typography>
+            <Typography varient="h5">Real Data</Typography>
+            <Typography colr="textSecondary">Real Date</Typography>
+            <Typography variant = "body2">Number of Active Cases of Covid-19</Typography>
+            </CardContent>
+            </Grid>
+            <Grid item component = {Card}>
+            <CardContent>
+            <Typography color = "textSecondary" gutterBottom>Recovered</Typography>
+            <Typography varient="h5">Real Data</Typography>
+            <Typography colr="textSecondary">Real Date</Typography>
+            <Typography variant = "body2">Number of Recovered Cases from Covid-19</Typography>
+            </CardContent>
+            </Grid>
+            <Grid item component = {Card}>
+            <CardContent>
+            <Typography color = "textSecondary" gutterBottom>Deaths</Typography>
+            <Typography varient="h5">Real Data</Typography>
+            <Typography colr="textSecondary">Real Date</Typography>
+            <Typography variant = "body2">Number of Deaths caused by Covid-19</Typography>
             </CardContent>
             </Grid>
         </Grid>
+        
         </div>
     )
 }
